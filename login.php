e78784f2fb018f718a5884d3896b5c8edfd06406
Firstname Lastname <pipearaya88@gmail.com>
2012-06-21 10:53:46 -0400

    archivo login

---
+++ login.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+
+</head>
+
+<body>
+
+<br/><br/>
+<center><h1>ingreso</h1></center>
+<form action="validar_registro.php" method="post">
+<table width="400" align="center">
+<tr>
+<td>Usuario: </td>
+<td><input name="username" type="text" id="username"></td>
+</tr>
+<tr>
+<td>Password: </td>
+<td><input name="password" type="password" id="password"></td>
+</tr>
+<tr>
+<td></td>
+<td><input name="" type="submit" value="Entrar"></td>
+</tr>
+</form>
+
+
+</body>
+</html>
+
+
